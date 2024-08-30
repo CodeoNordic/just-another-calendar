976950c8481890b4ae745fcc044e62ea766351b1
Vetle Lindbråten <vetlelind@gmail.com>
2024-08-30 10:36:43 +0200

    removed console logs

--- src/components/Calendar/Event/NewEvent.tsx
+++ src/components/Calendar/Event/NewEvent.tsx
@@ -2,7 +2,6 @@ import { useConfigState } from '@context/Config';
 import Crossmark from 'jsx:@assets/svg/crossmark.svg';
 
 
-
 interface NewEventProps {
     creatingEvent: boolean;
     setCreatingEvent: (value: boolean) => void;
@@ -32,12 +31,14 @@ const NewEvent: FC<NewEventProps> = props => {
                     <p className='createTitle'>Create Event?</p>
                     {config.newEventFields?.map(field => <div key={field.field}>
                         <p>{field.title ?? field.field}:</p>
-                        <input type={field.type ?? "string"} value={field.type === "time" ? newEvent?.[field.field].toString().split("T")[1] : newEvent?.[field.field] || ""} onChange={e => {
+                        <input 
+                            type={field.type ?? "string"} 
+                            value={field.type === "time" 
+                                ? newEvent?.[field.field].toString().split("T")[1] 
+                                : newEvent?.[field.field] || ""} onChange={e => {
                             let value = e.target.value as string | number | boolean;
                             field.type === "checkbox" && (value = e.target.checked);
                             field.type === "time" && (value = newEvent?.[field.field].toString().split("T")[0] + "T" + value);
-                            console.log(value);
-                            console.log(value.toString().split("T")[1]);
 
                             setNewEvent({...newEvent, [field.field]: value} as JAC.Event)}} />
                     </div>)}
@@ -47,12 +48,12 @@ const NewEvent: FC<NewEventProps> = props => {
                 <button onClick={() => {
                     setCreatingEvent(false);
                     setNewEvent(null);
-                }}>Cancel</button>
+                }}>Discard</button>
                 <button onClick={() => {
                     setCreatingEvent(false);
                     setNewEvent(null);
                     setConfig((prev: JAC.Config | null) => ({...prev, records: [...config.records, newEvent]} as JAC.Config));
-                }}>Create</button>
+                }}>Save</button>
             </div>
         </div>}
     </div>
