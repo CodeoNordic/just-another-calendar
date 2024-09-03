83ca89cb978cc43a08b7b9c11398ddc2c5fe4fdd
Vetle Lindbråten <vetlelind@gmail.com>
2024-09-03 08:46:04 +0200

    changed weekNumber func so no warnings show

--- src/utils/weekNumber.ts
+++ src/utils/weekNumber.ts
@@ -6,11 +6,31 @@ import { weekDays } from './calendarDates';
 // Source: https://www.w3resource.com/javascript-exercises/javascript-date-exercise-24.php
 export default function weekNumber(dt: Date) 
 {
+    const locale = window._config?.locale ?? 'en';
+    const firstDayOfWeek = typeof window._config?.firstDayOfWeek === 'string' ? 
+        Math.max(weekDays.indexOf(window._config?.firstDayOfWeek.toLowerCase().substring(0, 3)), 1) : 
+        window._config?.firstDayOfWeek ?? 1;
+
+    moment.updateLocale(locale, {
+        week: {
+            dow: firstDayOfWeek
+        }
+    });
+
+    return moment(dt).locale(locale).week();
+
+    /*
+    Old code, kept for now if needed
+
     moment.locale('no', {
         week: {
-            dow: typeof window._config?.firstDayOfWeek === 'string' ? Math.max(weekDays.indexOf(window._config?.firstDayOfWeek.toLowerCase().substring(0,3)), 1) : window._config?.firstDayOfWeek ?? 1
+            dow: typeof window._config?.firstDayOfWeek === 'string' ? 
+            Math.max(weekDays.indexOf(window._config?.firstDayOfWeek.toLowerCase().substring(0,3)), 1) : 
+            window._config?.firstDayOfWeek ?? 1
         }
     });
 
     return moment(dt).locale(window._config?.locale ?? 'en').week();
+
+    */
 }
