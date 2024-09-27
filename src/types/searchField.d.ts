66a22cf3682604720bb7548e59698f7f6a3ee434
Vetle Lindbråten <vetlelind@gmail.com>
2024-09-27 15:23:34 +0200

    fixed search and eventTemplates

---
+++ src/types/searchField.d.ts
@@ -0,0 +1,17 @@
+declare global {
+    namespace JAC {
+        interface SearchField {
+            searchBy?: string[]; // Fields from JAC.Event
+
+            title?: string; // Displayed over the search field
+            placeholder?: string; // Placeholder text for the search field
+            
+            openDefault?: boolean; // Whether the search field is open by default
+            search?: string; // The current search string
+
+            script?: string; // Script to run when the search field is changed for searching in FileMaker
+        }
+    }
+}
+
+export {};
